2f1063120e5f79adf8259b2df14b983a28c08581
Abhinav Singh <abhinav41999@gmail.com>
2025-07-03 21:46:05 +0530

    Update JavaForLoop.java

--- Java Practice/src/JavaForLoop.java
+++ Java Practice/src/JavaForLoop.java
@@ -3,6 +3,10 @@ public class JavaForLoop {
         simpleForLoop();
         System.out.println("***---------------------------------------***");
         sumNumberForLoop();
+        System.out.println("***---------------------------------------***");
+        nestedForLoop();
+        System.out.println("***---------------------------------------***");
+        nestedMultiplication();
     }
 
     private static void simpleForLoop(){
@@ -16,6 +20,26 @@ public class JavaForLoop {
         for(int i=0; i<10; i++){
             sum = sum+1;
             System.out.println("Sum of sum+1: "+sum);
+        }
+    }
+
+    public static void nestedForLoop(){
+        //Outer Loop
+        for(int i = 0; i<=2; i++){
+            System.out.println("Outer loop value of i: "+i);
+            //Inner Loop
+            for(int j = 0; j<=3; j++){
+                System.out.println("Inner loop value of j: "+j);
+            }
+        }
+    }
+
+    private static void nestedMultiplication(){
+        for(int i = 1; i <= 3; i++){
+            for(int j = 1; j <=3; j++){
+                System.out.println("i * j: "+(i*j));
+            }
+            System.out.println("");
         }
     }
 }
