9f7c9bbf28fb2298d3869cbfb0d87fbbc8a0c66b
Abhinav Singh <abhinav41999@gmail.com>
2025-07-10 23:34:25 +0530

    Update Car.java

--- Java Practice/src/JavaInheritance/Car.java
+++ Java Practice/src/JavaInheritance/Car.java
@@ -1,16 +1,22 @@
 package JavaInheritance;
 
 class Vehicle{
+    //Vehicle Attribute
     protected String Driver = "Max Verstappen";
+    //Vehicle Method
     public void honk(){
         System.out.println("Tutu Tutu Max Verstappen");
     }
 }
 public class Car extends Vehicle{
+    //Car Attribute
     private String teamName = "Red Bull Racing";
     public static void main(String[] args){
+        //Creating a myCar object
         Car myCar = new Car();
+        //Call the honk() method (from the Vehicle Class) on the myCar object
         myCar.honk();
+        //Display the value of the brand attribute (from the vehicle class) and the value of the modelName from the car class
         System.out.println(myCar.teamName+" : "+myCar.Driver);
     }
 }
