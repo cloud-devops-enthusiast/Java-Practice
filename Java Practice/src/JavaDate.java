bf302f546e46d7082d92747889937c597bc26982
Abhinav Singh <abhinav41999@gmail.com>
2025-07-16 16:16:03 +0530

    Create JavaDate.java

---
+++ Java Practice/src/JavaDate.java
@@ -0,0 +1,40 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class JavaDate {
+    public static void main(String[] args){
+        localDate();
+        System.out.println("***---------------------------------------***");
+        localTime();
+        System.out.println("***---------------------------------------***");
+        localDateTime();
+        System.out.println("***---------------------------------------***");
+        dateTimeFormatter();
+    }
+
+    public static void localDate(){
+        LocalDate myObj = LocalDate.now();
+        System.out.println("LocalDate: "+myObj);
+    }
+
+    public static void localTime(){
+        LocalTime myObj = LocalTime.now();
+        System.out.println("LocalTime: "+myObj);
+    }
+
+    public static void localDateTime(){
+        LocalDateTime myObj = LocalDateTime.now();
+        System.out.println("LocalDateTime: "+myObj);
+    }
+
+    public static void dateTimeFormatter(){
+        LocalDateTime myObj = LocalDateTime.now();
+        System.out.println("Before Formatting: "+myObj);
+        System.out.println("***---------------------------------------***");
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formatDate = myObj.format(myFormatObj);
+        System.out.println("After Formatting: "+formatDate);
+    }
+}
