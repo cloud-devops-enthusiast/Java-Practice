3c7ad36ecb44a02055a6866ea7ddd2b16d73ce84
Abhinav Singh <abhinav41999@gmail.com>
2025-07-18 23:55:40 +0530

    Create JavaListSorting.java

---
+++ Java Practice/Java Data Structures/src/JavaListSorting.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class JavaListSorting {
+    public static void main(String[] args){
+
+    }
+
+    public static void method1(){
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Honda");
+        cars.add("Mahindra");
+        cars.add("")
+    }
+}
