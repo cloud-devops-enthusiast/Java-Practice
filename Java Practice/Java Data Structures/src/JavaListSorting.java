c8e5992ee874419ed3636ef899588d19b6b53338
Abhinav Singh <abhinav41999@gmail.com>
2025-07-20 20:20:18 +0530

    commit

--- Java Practice/Java Data Structures/src/JavaListSorting.java
+++ Java Practice/Java Data Structures/src/JavaListSorting.java
@@ -8,6 +8,8 @@ public class JavaListSorting {
         method2();
         System.out.println("***---------------------------------------***");
         method3();
+        System.out.println("***---------------------------------------***");
+        method4();
     }
 
     public static void method1(){
@@ -62,6 +64,22 @@ public class JavaListSorting {
         myNumbers.add(49);
         //Sort myNumber
         Collections.sort(myNumbers);
+        //Printing out elements from the Arraylist
+        for (int i: myNumbers){
+            System.out.println("myNumber: "+i);
+        }
+    }
+
+    public static void method4(){
+        ArrayList<Integer> myNumbers = new ArrayList<Integer>();
+        //Adding elements to Arraylist
+        myNumbers.add(95);
+        myNumbers.add(61);
+        myNumbers.add(62);
+        myNumbers.add(24);
+        myNumbers.add(49);
+        //Sort ArrayList myNumber in reverse order
+        Collections.sort(myNumbers, Collections.reverseOrder());
         //Printing out elements from the Arraylist
         for (int i: myNumbers){
             System.out.println("myNumber: "+i);
---
+++ Java Practice/Java Data Structures/src/JavaSetInterface.md
@@ -0,0 +1,13 @@
+The Set is part of the Java Collection Framework and is used to store a collection of unique elements.
+Unlike a List, a Set does not allow duplicates, and it does not preserve the order of elements (Unless you're using TreeSet or LinkedHashList).
+Common classes that implement Set:
+- HashSet: fast and unOrdered
+- TreeSet: sorted set
+- LinkedHashSet: Ordered by insertion
+
+Common Set Methods:
+- add(): Adds an element if it's not already in the set
+- remove(): Removes the element from the set
+- contains(): Checks if the set contains the element
+- size(): Returns the number of elements
+- clear(): Removes all elements
