1e4608cecd55158a2c3cc99eef59d8135e20a428
Abhinav Singh <abhinav41999@gmail.com>
2025-07-25 23:43:16 +0530

    Update JavaFibonacci.java

--- Java Practice/Java Data Structures/src/JavaFibonacci.java
+++ Java Practice/Java Data Structures/src/JavaFibonacci.java
@@ -1,10 +1,10 @@
 public class JavaFibonacci {
     public static void main(String[] args){
         method1();
-        System.out.println("*****-------------*****");
-        System.out.println(0);
-        System.out.println(1);
-        method2(1,0);
+        System.out.println("*****----------------*****");
+        method2(0,1,1);
+        System.out.println("*****----------------*****");
+        method4();
     }
 
     public static void method1(){
@@ -13,7 +13,7 @@ public class JavaFibonacci {
         int var2 = 0;
         int var1 = 1;
 
-        //Checking out of values of Variables
+        //Checking out of values of Variableszxsedr2
         System.out.println("Variable 2: "+var2);
         System.out.println("Variable 1: "+var1);
 
@@ -29,19 +29,46 @@ public class JavaFibonacci {
         }
     }
 
-    public static void method2(int prev1, int prev2){
-        //Creating a new variable count
-        int count = 2;
-        if (count <= 19) {
+    public static void method2(int prev1, int prev2, int count){
+        //if you declare variable count the output will overflow and break the whole process
+        //Checking the value of variable count is less than or equals to 10
+        if (count <= 10) {
+            //Declaring and assigning value to newFibo to create a fibonacci series
+            //As fibonacci pattern is about the addition of last two results
             int newFibo = prev1 + prev2;
-            System.out.println(newFibo);
+            System.out.println("Count: "+count+" Fibonacci No: "+newFibo);
+            //Assiging prev1 value to prev 2 so it can be passed on next time
             prev2 = prev1;
+            //Assigning newFibo which is a new fibonacci just created so it can be used in next iteration while adding with the prev2 number.
             prev1 = newFibo;
-            count += 1;
             //method calling itself
-            method2(prev1, prev2);
+            //Recursion
+            method2(prev1, prev2, count+1);
         } else {
             return;
         }
+    }
+
+    public static int method3(int n){
+        //Finding the nth Fibonacci Number using Recursion
+        //This means the Fibonacci Number of a certain number which you pass at any given nth position
+        //F(n) = F(n-1)+F(n-2) (Here n is like the 10th Fibonacci Number which is the sum of 8th and 9th Fibonacci Numbers)
+        //Printing out the value of (n-1) and (n-2)
+        if(n <= 1){
+            return n;
+        }
+        else {
+            //Applying the formula F(n)=F(n-1)+F(n-2)
+            return method3(n-1) + method3(n-2);
+        }
+    }
+
+    public static void method4(){
+        int n = 19;
+        //Printing out the value of (n-1) and (n-2)
+        System.out.println("n-1: "+(n-1));
+        System.out.println("n-2: "+(n-2));
+        //like here is the addition of 17th and 18th Fibonacci Numbers
+        System.out.println("Addition of the last two number of Fibonacci Series: "+method3(n));
     }
 }
