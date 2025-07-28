ea4089b77c27f474e8b00728c8a7d5741e406c2a
Abhinav Singh <abhinav41999@gmail.com>
2025-07-28 23:59:24 +0530

    Update Java_SelectionSort.java

--- Java Practice/Java Data Structures/src/Java_SelectionSort.java
+++ Java Practice/Java Data Structures/src/Java_SelectionSort.java
@@ -1,6 +1,8 @@
 public class Java_SelectionSort {
     public static void main(String[] args){
         method1();
+        System.out.println("*****----------------*****");
+        method2();
     }
 
     public static void method1(){
@@ -39,6 +41,36 @@ public class Java_SelectionSort {
     }
 
     public static void method2(){
+        //Declaring an integer type array
+        int[] myArray = {99, 17, 19, 37, 92, 87, 77, 10, 29, 39};
+        //Assigning the length of array to an integer variable
+        int myArrayLen = myArray.length;
 
+        //Outer Loop for running through once for each element in the array
+        for (int i = 0 ; i < myArrayLen ; i++){
+            //This variable will be used to keep the index of the smallest value found
+            int minIndex = i;
+            //This for loop is for searching the unsorted portion of the array (from 'i+1' to 'n-1')
+            for (int j = i + 1 ; j < myArrayLen ; j++){
+                //Comparing and Updating minIndex with j
+                //This will update if the element at j is smaller than the element at minIndex
+                if(myArray[j] < myArray[minIndex]) {
+                    //minIndex will always hold the index of the smallest value in the unsorted part
+                    minIndex = j;
+                }
+            }
+            //Swapping Logic
+            //Storing the value of i element to a temp variable
+            int temp = myArray[i];
+            //Swapping the smallest value found at minIndex
+            myArray[i] = myArray[minIndex];
+            //Swapping the value with the swapped value
+            myArray[minIndex] = temp;
+        }
+
+        System.out.println("Sorted Array: ");
+        for (int i = 0 ; i < myArrayLen ; i++){
+            System.out.println(myArray[i] + " ");
+        }
     }
 }
