bbb0614bac05683d0e3b95ce553646fdc459d8cf
Abhinav Singh <abhinav41999@gmail.com>
2025-08-19 23:56:53 +0530

    QuickSort

---
+++ Java Practice/Java Data Structures/src/DSA_QuickSort.md
@@ -0,0 +1,8 @@
+**Quick Sort**
+
+Quick Sort is one of the fastest sorting algorithms. 
+
+Quicksort algorithm takes an array as input and considers one of the element as pivot element. It chooses one element as the pivot element and moves the other values to the left of the pivot element and higher values to the right of the element.
+
+Consider a pack of cards with different numbers written on them, so you take one card out of them which acts as a divider, so now you move all the cards with small numbers to the left and the larger numbers to the right, now sort the left and right sections of cards doing the sorting same way. At last repeat it with every section so at end you will only have one card left.
+
--- Java Practice/Java Data Structures/src/Java_QuickSort.java
+++ Java Practice/Java Data Structures/src/Java_QuickSort.java
@@ -1,2 +1,39 @@
 public class Java_QuickSort {
+    public static void main(String[] args){
+        int[] myArray = {62, 43, 52, 32, 12, 22, 21, 93, 4};
+        quickSort(myArray, 0, myArray.length-1);
+
+        System.out.println("Sorted Array: ");
+        for(int value : myArray){
+            System.out.println(value + " ");
+        }
+    }
+
+    public static void quickSort(int[] myArray, int low, int high){
+        if(low<high){
+            int pivotIndex = partition(myArray, low, high);
+            quickSort(myArray, low, pivotIndex-1);
+            quickSort(myArray, pivotIndex+1, high);
+        }
+    }
+
+    public static int partition(int[] myArray, int low, int high){
+        int pivot = myArray[high];
+
+        int i = low - 1;
+
+        for(int j = low; j < high; j++){
+            if(myArray[j] <= pivot){
+                i++;
+                int temp = myArray[i];
+                myArray[i] = myArray[j];
+                myArray[j] = temp;
+            }
+        }
+
+        int temp = myArray[i + 1];
+        myArray[i + 1] = myArray[high];
+        myArray[high] = temp;
+        return i + 1;
+    }
 }
