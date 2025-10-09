e4a885c661c8b1d16b7d7ee6c9308d2d655bc5f2
Abhinav Singh <abhinav41999@gmail.com>
2025-10-09 21:18:30 +0530

    Update JavaVariables.java

--- Java Practice/src/JavaVariables.java
+++ Java Practice/src/JavaVariables.java
@@ -4,10 +4,15 @@ public class JavaVariables {
     }
 
     public static void print(){
+        //This is a string data type which can store group of characters
         String name = "Abhinav";
+        //This is an integer data type which can store numeric values
         int passCode = 6969;
+        //This is a float datatype which stores decimal values to it
         float weight = 74.7F;
+        //This is a character data type which can store a single character value
         char initial = 'a';
+        //This is a boolean data type which can store only two things either true or false.
         boolean testCall = true;
         System.out.println("Name: "+name);
         System.out.println("PassCode: "+passCode);
