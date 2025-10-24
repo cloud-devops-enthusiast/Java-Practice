82f5121865cef2cba8c276644def0771e0da64be
Abhinav Singh <abhinav41999@gmail.com>
2025-10-24 14:24:48 +0530

    Committ

--- Java Practice/src/JavaScanner.java
+++ Java Practice/src/JavaScanner.java
@@ -1,10 +1,18 @@
 //Importing the scanner class
 import java.util.Scanner;
+//Writing String to a file
+//Importing the File class
+import java.io.File;
+//Importing this class to handle the errors
+import java.io.FileNotFoundException;
 
 public class JavaScanner {
 
     public static void main(String[] args){
+        //Using simple scanner object
         Print();
+        //Using a method to read out the contents from a file
+        printFile();
     }
 
     public static void Print(){
@@ -25,6 +33,29 @@ public class JavaScanner {
         }
         else {
             System.out.println("The User you have entered doesn't have access to the system.");
+        }
+    }
+
+    public static void printFile(){
+        //Creating try catch block for effective error handelling
+        try {
+            //Creating object of the file
+            File myObj = new File("/Users/abhinav/Developer/Java-Practice/Java Practice/src/Test.txt");
+            //Creating object for the scanner class
+            Scanner myScanner = new Scanner(myObj);
+            //Checking for data in the file
+            while ((myScanner.hasNextLine())){
+                String data = myScanner.nextLine();
+                //Printing out the file data line by line
+                //There is backlog of this feature that it will print out even the blank lines as well
+                System.out.println("Data: "+data);
+            }
+            //Close method is called here when reading of data is completed from the file
+            myScanner.close();
+        } catch (FileNotFoundException e) {
+            //Error Handelling
+            System.out.println("Some Error has been occured.");
+            throw new RuntimeException(e);
         }
     }
 }
