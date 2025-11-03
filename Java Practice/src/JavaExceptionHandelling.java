1461154cb1585f79a4771e8dbf798da6157341d4
Abhinav Singh <abhinav41999@gmail.com>
2025-11-04 00:11:12 +0530

    Commit

---
+++ Java Practice/src/JavaExceptionHandelling.java
@@ -0,0 +1,36 @@
+public class JavaExceptionHandelling {
+    public static void main(String args[]){
+        //Testing the normal Usecase
+        System.out.println("Normal Usecase: ");
+        normalUsecase();
+        System.out.println("Exception Handelled: ");
+        exceptionHandelled();
+        System.out.println("Exception Issue: ");
+        exceptionIssue();
+    }
+
+    public static void exceptionIssue(){
+        //In this case we are looking for creating a exception.
+        int[] myNumbers = {1,2,3};
+        System.out.println("Highest Number in an Array: "+myNumbers[10]);//Error
+    }
+
+    public static void exceptionHandelled(){
+        //In this case we are in handelling exception handelling using the try catch block
+        try{
+            //The suspected dark code goes here.
+            int[] myNumbers = {1,2,3};
+            System.out.println("Highest Number in an Array: "+myNumbers[10]);
+        }
+        catch(Exception e){
+            //The Solution or clarification of Code goes here
+            System.out.println("Something went wrong while finding the resuls.");
+        }
+    }
+
+    public static void normalUsecase(){
+        //This is a normal method testing the code in a simple manner.
+        int[] myNumber = {1,4,8};
+        System.out.println("Printing the Numbers: "+myNumber[2]);
+    }
+}
---
+++ Java Practice/src/JavaExceptionHandelling.md
@@ -0,0 +1,20 @@
+**Java Exception Handelling**
+
+Whenever an exception happens, Java will normally Stop and Generate an error message.
+
+This is mostly done with the code which is suspected to throw an error or exception.
+
+There are some keywords which are used in exception handelling like:
+
+*Try:* The try statement allows you to define a block of code to be tested for errors while it is being excecuted.
+
+*Catch:* The catch statement allows you to define a block of code to be executed, if an error occurs in the try statement.
+
+**Below mentioned is the syntax of Exception Handling in java.**
+            try{
+
+            }
+            catch{
+
+            }
+
